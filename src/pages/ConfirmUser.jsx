e3ed538d2c911a7453469a927d464ced98db2d7f
Juan Coronado <coronajh19@gmail.com>
2024-08-05 18:48:15 +0200

    Pagina lista de confirmar y modificaciones del feedback del usuario

--- src/pages/ConfirmUser.jsx
+++ src/pages/ConfirmUser.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import UsersLayout from "../layouts/UsersLayout.jsx";
+import ConfirmUserLayout from "../layouts/ConfirmUserLayout.jsx";
 
 const ConfirmUser = () => {
   const { validationCode } = useParams();
   const navigate = useNavigate();
   const [message, setMessage] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState(false);
+
   const confirmUser = async () => {
     try {
       const response = await fetch(
@@ -19,41 +20,51 @@ const ConfirmUser = () => {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const responseData = await response.json();
-      setMessage(responseData.message);
+      setMessage("¡Todo listo! Ya puedes iniciar sesión para comenzar.");
+      setError(false); // Confirmación exitosa
     } catch (error) {
-      setError("Error al confirmar usuario: " + error.message);
+      setMessage("Error: el token es erróneo o ha caducado."); // Mensaje de error
+      setError(true); // Error al confirmar
     }
   };
+
   useEffect(() => {
     confirmUser();
   }, []);
 
   return (
-    <UsersLayout componente={"Confirmar Usuario"}>
-      <div className="flex flex-col items-center w-full h-full justify-evenly content-center text-base sm:text-lg md:text-3xl">
-        {error ? (
-          <div className="text-red-500">
-            <h2>{error}</h2>
+    <ConfirmUserLayout>
+      <div className="flex flex-col h-full justify-between text-center text-black">
+        <div className="text-center">
+          {error ? (
+            <div className="text-red-500">
+              <h3>{message}</h3>
+            </div>
+          ) : (
+            <div className="text-[4vw] sm:text-[24px] md:text-[32px]">
+              <h3>{message}</h3>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col items-center">
+          {error ? (
             <button
               onClick={() => navigate("/signup")}
-              className="bg-black hover:bg-teal-500 text-white font-bold py-2 px-5 rounded-lg mt-4"
+              className="bg-black hover:bg-teal-500 text-white font-bold py-2 px-5 rounded-lg mt-4 uppercase"
             >
               Volver a Registrarse
             </button>
-          </div>
-        ) : (
-          <div className="text-green-500">
-            <h2>{message}</h2>
+          ) : (
             <button
               onClick={() => navigate("/login")}
-              className="bg-black hover:bg-teal-500 text-white font-bold py-2 px-5 rounded-lg mt-4"
+              className="bg-black hover:bg-teal-500 text-white font-bold py-2 px-5 rounded-lg mt-4 uppercase"
             >
               Iniciar Sesión
             </button>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </UsersLayout>
+    </ConfirmUserLayout>
   );
 };
 
