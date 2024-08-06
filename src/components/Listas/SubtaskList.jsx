3e668a1e2e54a03d55758cd8cab97f171dd6d30d
Robert Muresan <robotzelu89@gmail.com>
2024-08-06 13:57:49 +0100

    algo mas de tailwind

--- src/components/Listas/SubtaskList.jsx
+++ src/components/Listas/SubtaskList.jsx
@@ -31,20 +31,20 @@ const SubtaskList = ({subtasks, handleDeleteSubtask, handleToggleSubtask }) => {
     
      <div className="flex flex-col col-span-4 items-center bg-amber-100 rounded-bl-lg p-4 md:col-span-8 w-3/5">
       
-      <div className="pt-10 pb-6 flex flex-col items-center bg-amber-950 border-solid border-2 border-black rounded-t-3xl size-42"> 
+      <div className="pt-8 pb-6 flex flex-col items-center bg-amber-950 border-solid border-2 border-black border-b-0 rounded-t-3xl size-42"> 
         <h2 className="flex flex-col text-center text-3xl mx-14 font-bold text-white ">Lista</h2>
       </div>
-      <div className=" items-center bg-amber-950 border-solid border-2 border-black">
-      {
-          subtasks.length > 1 ? <section className="mb-4">
+      <div className=" items-center bg-amber-950 border-solid border-2 border-black rounded-lg">
+    
+          <section className="mb-4">
             
             <Select className=" m-10 items-center"
               onChange={option => setSortBy(option.value)} 
               defaultValue={sortingOptions[0]} 
               options={sortingOptions} />
-          </section> : null
-        }
-      <ul className=" items-center bg-amber-50 shadow-lg rounded-lg p-6 border-2 border-amber-300 min-h-[50vh] max-h-[70vh] overflow-y-auto">
+          </section> 
+      
+      <ul className=" items-center m-5 bg-amber-50 shadow-lg rounded-lg p-6 border-2 border-amber-300 min-h-[50vh] max-h-[70vh] overflow-y-auto">
         
         {
         sortedSubtasks.map((subtask) => {
